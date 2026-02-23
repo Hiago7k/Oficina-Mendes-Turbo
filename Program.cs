b3a0f535bd1b1d2f3d6cef25829e83cf3585b9f3
Hiago 7k <hiago7k@gmail.com>
2026-02-23 20:32:39 -0300

    ajustando estrutura do sistema

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("==============================");
+Console.WriteLine("*******OFICINA MENDES********");
+Console.WriteLine("==============================");
+
+
+Console.WriteLine("\nBem vindo! Como nos Podemos lhe ajudar hoje?");
+
+
+Console.WriteLine("1. Marcar uma Avaliacao: ");
+Console.WriteLine("2. Solicitar um Servico: ");
+Console.WriteLine("3. Limpeza de Veiculo: ");
+Console.WriteLine("4. Encerrar Auto Atendimento: ");
+int resposta = Convert.ToInt32(Console.ReadLine()!);
+
+switch (resposta)
+{
+    case 1:
+        // Marcar Avaliacao
+        Console.Clear();
+        Console.WriteLine("");
+        break;
+    case 2:
+        // Solicitar Servico
+        break;
+    case 3:
+        // Limpeza de Veiculo
+        break;
+    case 4:
+        // Encerar chat
+       break;
+    default:
+        // Opcao Invalida
+        break;
+}
