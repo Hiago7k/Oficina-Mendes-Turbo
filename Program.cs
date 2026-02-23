5dd92d17cf582ac2a2d36c2c6b2963cbdfc32436
Hiago 7k <hiago7k@gmail.com>
2026-02-23 20:59:46 -0300

    ajustando ordem

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ Console.WriteLine("==============================");
 Console.WriteLine("*******OFICINA MENDES********");
 Console.WriteLine("==============================");
 
+Console.WriteLine("\nPara continuar seu atendimento, preencha seus dados abaixo!");
+Console.WriteLine("Digite seu Nome Completo: ");
+string nomeDoCliente = Console.ReadLine()!;
+Console.WriteLine("Digite seu CPF: ");
+string cpfDoCliente = Console.ReadLine()!;
+Console.WriteLine("Digite seu Numero de Telefone com o dd (00) 1234-2456");
+string numeroDoCliente = Console.ReadLine()!;
+
+Cliente cliente = new Cliente(nomeDoCliente, cpfDoCliente, numeroDoCliente);
+
+
+
 
 Console.WriteLine("\nBem vindo! Como nos Podemos lhe ajudar hoje?");
 
@@ -17,20 +29,6 @@ int resposta = Convert.ToInt32(Console.ReadLine()!);
 switch (resposta)
 {
     case 1:
-        // Marcar Avaliacao
-        Console.Clear();
-        Console.WriteLine("Para continuar seu atendimento, preencha seus dados abaixo!");
-        Console.WriteLine("Digite seu Nome Completo: ");
-        string nomeDoCliente = Console.ReadLine()!;
-        Console.WriteLine("Digite seu CPF: ");
-        string cpfDoCliente = Console.ReadLine()!;
-        Console.WriteLine("Digite seu Numero de Telefone com o dd (00) 1234-2456");
-        string numeroDoCliente = Console.ReadLine()!;
-
-
-        Cliente cliente = new Cliente(nomeDoCliente, cpfDoCliente, numeroDoCliente);
-        Console.WriteLine($"{cliente.Nome} {cliente.CPF} {cliente.NumeroDeTelefone}");
-
         // Avaliacao
         Console.WriteLine("Qual o Veiculo: ");
         string veiculo = Console.ReadLine()!;
@@ -38,13 +36,19 @@ switch (resposta)
         string placa = Console.ReadLine()!;
 
         Avaliacao avaliacao = new Avaliacao(veiculo, placa, cliente);
-        Console.WriteLine();
+        Console.Clear();
+        Console.WriteLine("Seus dados abaixo...");
         Console.WriteLine($" {avaliacao.Veiculo} {avaliacao.PlacaDoVeiculo} {avaliacao.Cliente.Nome} {avaliacao.Cliente.CPF}");
 
+        Console.WriteLine("Obrigado por nos escolhers um Atendente da nossa equipe, entrara em contato em-breve  atraves do seu numero para agendar essa avaliacao");
 
         break;
     case 2:
         // Solicitar Servico
+        Console.Clear();
+
+        
+        
         break;
     case 3:
         // Limpeza de Veiculo
@@ -56,3 +60,4 @@ switch (resposta)
         // Opcao Invalida
         break;
 }
+
